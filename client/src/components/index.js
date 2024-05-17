35636dd8aef0633a44730c11d7c70cd827e6f611
achraf.melkii <achraf.melkii@gmail.com>
2024-05-17 10:35:46 +0100

    Customiser added

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,9 @@
-import CustomButton from "./CustomButton";
 
-export {CustomButton} ;
+import CustomButton from "./CustomButton";
+import AIPicker from "./AIPicker";
+import ColorPicker from "./ColorPicker";
+import filepicker from "./FilePicker";
+import Tab from "./Tab";
+
+
+export {CustomButton,AIPicker,ColorPicker,filepicker,Tab} ;
